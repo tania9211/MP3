2b07134404b1bf086f042fcd57cca5c9ffe13119
tatianaBeliaieva <tatianaBeliaieva@tatianaievasmbp.dlink>
2015-02-11 17:08:28 +0200

    UI was changed

--- songReader/angularModule.js
+++ songReader/angularModule.js
@@ -28,18 +28,21 @@ app.controller("testController", function ($scope) {
 });
 
 var fileList;
+var getInfoButtonElements = document.getElementsByClassName("getInfoButton");
 function getInfo() {
     songInfoApplet.popUpFileChooser();
     fileList = JSON.parse(document.songInfoApplet.getFilePath());
 }
 
 app.controller('appletController', ['$scope', function ($scope) {
-    document.getElementById("getInfoButton").addEventListener('click', function () {
-        $scope.$apply(function () {
-            getInfo();
-            $scope.fileListArray = fileList;
+    for (var i = 0; i < getInfoButtonElements.length; i++) {
+        getInfoButtonElements[i].addEventListener('click', function () {
+            $scope.$apply(function () {
+                getInfo();
+                $scope.fileListArray = fileList;
+            });
         });
-    });
+    }
 
     $scope.tableElementsArray = [{columnName: 'Song', columnValue: '', columnType: 'TIT2'}, {
         columnName: 'Album',
@@ -52,9 +55,26 @@ app.controller('appletController', ['$scope', function ($scope) {
             columnType: 'TYER'
         }, {columnName: 'Genre', columnValue: '', columnType: 'TCON'}];
 
+    $scope.songPath;
+
     $scope.callSongInfo = function (name) {
         var songInfo = JSON.parse(document.songInfoApplet.getSongInfo(name));
-      //   alert("Info " + songInfo.TIT2);
+        if (songInfo.TIT2 == "null") {
+            alert("TIT2");
+        }
+        if (songInfo.TALB == "null") {
+            alert("TALB");
+        }
+        if (songInfo.TPE2 == "null") {
+            alert("TPE2");
+        }
+        if (songInfo.TYER == "null") {
+            alert("TYER");
+        }
+        if (songInfo.TCON == "null") {
+            alert("TCON");
+        }
+
         $scope.tableElementsArray = [{
             columnName: 'Song',
             columnValue: songInfo.TIT2,
@@ -65,22 +85,14 @@ app.controller('appletController', ['$scope', function ($scope) {
                 columnValue: songInfo.TYER,
                 columnType: 'TYER'
             }, {columnName: 'Genre', columnValue: songInfo.TCON, columnType: 'TCON'}];
+
+        $scope.songPath = name;
     };
 
     $scope.saveSongInfomation = function () {
-        songInfoApplet.setSongInformation(JSON.stringify($scope.tableElementsArray));
+        songInfoApplet.setSongInformation($scope.songPath, JSON.stringify($scope.tableElementsArray));
     };
 }]);
-
-/*var fileList;
- function getInfo() {
- console.log(fileList);
- songInfoApplet.popUpFileChooser();
- console.log("2");
- fileList = JSON.parse(document.songInfoApplet.getFilePath());
- console.log("3");
- }*/
-
 
 /*document.getElementById("songsList") = function(e) {
  window.scrollTo(document.body.scrollLeft,
